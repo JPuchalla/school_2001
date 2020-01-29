868266cf75d9c87a618ca0666bdabe34c0c4b7b9
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-01-29 13:58:26 -0700

    Pushing portion of Iteration 1

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,12 @@
 class School
-  attr_reader :time, :name
+  attr_reader :start_time, :total_time
   def initialize(name_parameter, time)
-    @name  = name_parameter
-    @time = time
+    @start_time  = name_parameter
+    @total_time = time
   end
 
+  def hours_in_school_day
+    @total_time = test_it_has_hours_in_school_day
+
+  end
 end
